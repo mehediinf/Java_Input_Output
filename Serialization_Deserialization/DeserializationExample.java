c14efbb94430ba9996a80cc41545b21b0690234f
Mehedi Hasan <mehediinform@gmail.com>
2024-09-02 00:30:03 +0600

    10th Commit

--- Serialization_Deserialization/DeserializationExample.java
+++ Serialization_Deserialization/DeserializationExample.java
@@ -1,12 +1,64 @@
 package Serialization_Deserialization;
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class DeserializationExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream("person.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            Animal animal = new Animal("Hasan", 25);
+            oos.writeObject(animal);
+
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         
 
+        Animal animal1 = null;
+        try (FileInputStream fileIn = new FileInputStream("person.ser");
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+                animal1 = (Animal) in.readObject();
 
+                animal1.display();
 
+             }
+        catch(Exception e){
 
+        }
     }
+}
+
+/**
+ * Animal
+ */
+class Animal implements Serializable{
+
+    private String name;
+    private int age;
+
+    public Animal(String name,int age){
+
+        this.name = name;
+        this.age = age;
+    }
+
+    public void display(){
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+    }
+    
+
+
+
 }
--- Serialization_Deserialization/SerializationExample.java
+++ Serialization_Deserialization/SerializationExample.java
@@ -1,12 +1,62 @@
 package Serialization_Deserialization;
-
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 public class SerializationExample{
 public static void main(String[] args) {
     
-r
+    Person person = new Person("Mehedi", 24,"CSE");
+        
+        try (FileOutputStream fileOut = new FileOutputStream("Serialization_Deserialization/person.ser");
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+            out.writeObject(person);
+            System.out.println("Serialized data is saved in person.ser");
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
 
-
+        Person person1 = null;
+        try (FileInputStream fileIn = new FileInputStream("Serialization_Deserialization/person.ser");
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+            person1 = (Person) in.readObject();
+            
+            System.out.println("Deserialized Person...");
+            System.out.println("Name: " + person1.getName());
+            System.out.println("Age: " + person1.getAge());
+            System.out.println("Dept: "+person1.getDept());
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
 }
+}
+
+
+class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private String name;
+    private int age;
+    private String dept;
+
+    public Person(String name, int age,String dept) {
+        this.name = name;
+        this.age = age;
+        this.dept = dept;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public String getDept() {
+        return dept;
+    }
 }
